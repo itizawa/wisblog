f22c9528b2ef6a0a4754374f96a26694953e4e5d
itizawa <itizawa.pen@gmail.com>
2024-08-11 22:39:51 +0900

    Support: biome を使った linter に修正した

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,22 +1,22 @@
-import { json, urlencoded } from "body-parser";
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import { json, urlencoded } from 'body-parser'
+import cors from 'cors'
+import express from 'express'
+import morgan from 'morgan'
 
 export const createServer = () => {
-	const app = express();
-	app
-		.disable("x-powered-by")
-		.use(morgan("dev"))
-		.use(urlencoded({ extended: true }))
-		.use(json())
-		.use(cors())
-		.get("/message/:name", (req, res) => {
-			return res.json({ message: `hello ${req.params.name}` });
-		})
-		.get("/status", (_, res) => {
-			return res.json({ ok: true });
-		});
+  const app = express()
+  app
+    .disable('x-powered-by')
+    .use(morgan('dev'))
+    .use(urlencoded({ extended: true }))
+    .use(json())
+    .use(cors())
+    .get('/message/:name', (req, res) => {
+      return res.json({ message: `hello ${req.params.name}` })
+    })
+    .get('/status', (_, res) => {
+      return res.json({ ok: true })
+    })
 
-	return app;
-};
+  return app
+}
