cbe3bce11935bfe3ee1ad7d52c18ec183a2fd980
itizawa <itizawa.pen@gmail.com>
2024-08-18 20:22:24 +0900

    Fix: フォーマットを実施した

--- packages/jest-presets/node/jest-preset.js
+++ packages/jest-presets/node/jest-preset.js
@@ -6,4 +6,4 @@ module.exports = {
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   modulePathIgnorePatterns: ['<rootDir>/test/__fixtures__', '<rootDir>/node_modules', '<rootDir>/dist'],
   preset: 'ts-jest',
-}
+};
