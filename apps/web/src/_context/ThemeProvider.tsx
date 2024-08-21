929fca869687805943696be62ab8695fcac6ce38
itizawa <itizawa.pen@gmail.com>
2024-08-21 21:10:22 +0900

    Change: 設定値の整備

---
+++ apps/web/src/_context/ThemeProvider.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider as MuiThemeProvider, type PaletteColor, alpha, createTheme } from '@mui/material/styles';
+import { Roboto } from 'next/font/google';
+import type React from 'react';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    gray: PaletteColor;
+  }
+  interface PaletteOptions {
+    gray: PaletteColor;
+  }
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    gray: true;
+  }
+}
+
+export const ThemeProvider = (props: { children: React.ReactNode }) => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: roboto.style.fontFamily,
+    },
+    palette: {
+      mode: 'dark',
+      gray: {
+        main: '#A5A4A8',
+        light: alpha('#A5A4A8', 0.5),
+        dark: alpha('#A5A4A8', 0.9),
+        contrastText: '#fff',
+      },
+    },
+  });
+
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      {props.children}
+    </MuiThemeProvider>
+  );
+};
