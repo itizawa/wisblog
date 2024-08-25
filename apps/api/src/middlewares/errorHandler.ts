c210d52a0d7da79f2b2c27be10098ce7b8b2f6eb
itizawa <itizawa.pen@gmail.com>
2024-08-25 21:21:23 +0900

    Change: 500エラーの場合も正しくハンドリングされるようにした

--- apps/api/src/middlewares/errorHandler.ts
+++ apps/api/src/middlewares/errorHandler.ts
@@ -2,6 +2,9 @@ import { ValidationError } from '@repo/types';
 import type { NextFunction, Request, Response } from 'express';
 
 export const errorHandler = (err: unknown, _req: Request, res: Response, _next: NextFunction) => {
+  // TODO: ロギングライプラリを使う https://github.com/itizawa/wisblog/issues/42
+  console.info(err);
+
   if (err instanceof ValidationError) {
     return res.status(400).json({ message: err.message });
   }
