65644bf27aba34741c118d10d7d3311bfa885753
itizawa <itizawa.pen@gmail.com>
2024-09-15 19:02:13 +0900

    Add: スキーマの追加

--- packages/types/src/domains/inputTypeSchemas/ArticleScalarFieldEnumSchema.ts
+++ packages/types/src/domains/inputTypeSchemas/ArticleScalarFieldEnumSchema.ts
@@ -1,5 +1,13 @@
 import { z } from 'zod';
 
-export const ArticleScalarFieldEnumSchema = z.enum(['id', 'title', 'body', 'authorId', 'createdAt', 'updatedAt']);
+export const ArticleScalarFieldEnumSchema = z.enum([
+  'id',
+  'title',
+  'body',
+  'blogId',
+  'authorId',
+  'createdAt',
+  'updatedAt',
+]);
 
 export default ArticleScalarFieldEnumSchema;
--- packages/types/src/domains/modelSchema/ArticleSchema.ts
+++ packages/types/src/domains/modelSchema/ArticleSchema.ts
@@ -10,6 +10,7 @@ export const ArticleSchema = z.object({
     .string({ required_error: '名前を入力してください' })
     .max(255, { message: '名前は255文字未満で入力してください' }),
   body: z.string(),
+  blogId: z.string(),
   authorId: z.string(),
   createdAt: z.coerce.date(),
   updatedAt: z.coerce.date(),
