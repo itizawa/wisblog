3663bd1e5f6ffc8a2eaef22d1d2423eee9c14610
itizawa <itizawa.pen@gmail.com>
2024-09-15 11:23:34 +0900

    Change: バリデーションエラーを表示するために prisma ファイルを修正した

--- packages/types/src/domains/modelSchema/BlogSchema.ts
+++ packages/types/src/domains/modelSchema/BlogSchema.ts
@@ -6,7 +6,9 @@ import { z } from 'zod';
 
 export const BlogSchema = z.object({
   id: z.string(),
-  name: z.string().max(255, { message: '名前は255文字未満で入力してください' }),
+  name: z
+    .string({ required_error: '名前を入力してください' })
+    .max(255, { message: '名前は255文字未満で入力してください' }),
   subDomain: z.string().min(3, { message: 'サブドメインは3文字以上で入力してください' }),
   ownerId: z.string(),
   createdAt: z.coerce.date(),
