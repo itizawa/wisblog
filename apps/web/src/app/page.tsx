1c5c83769ab59dce35cd7de574b592d1a6a6d30e
itizawa <itizawa.pen@gmail.com>
2024-09-15 16:48:00 +0900

    Change: トップページにダッシュボードへの動線を追加した

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,9 +1,26 @@
-'use client';
+import { Link, Stack, Typography } from '@mui/material';
+import urlJoin from 'url-join';
+import { getCurrentUser } from '~/actions/user';
 
-export default function Page() {
+export default async function Page() {
+  const { currentUser } = await getCurrentUser();
+
   return (
-    <div>
-      <h1>Web</h1>
-    </div>
+    <Stack maxWidth={600} mx='auto' pt={4} gap={3}>
+      <Typography variant='h5'>Welcome to Wisblog !</Typography>
+      {currentUser ? (
+        <Link href={'/dashboards/blogs'} color='info' sx={{ textDecoration: 'none' }}>
+          <Typography variant='h5'>ダッシュボードへ</Typography>
+        </Link>
+      ) : (
+        <Link
+          href={urlJoin(process.env.NEXT_PUBLIC_API_HOST || 'http://localhost:8080', '/users/auth/google')}
+          color='info'
+          sx={{ textDecoration: 'none' }}
+        >
+          <Typography variant='h5'>ログイン</Typography>
+        </Link>
+      )}
+    </Stack>
   );
 }
