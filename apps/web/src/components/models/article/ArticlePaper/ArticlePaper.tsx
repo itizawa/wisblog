dd8d619949444c977c9f65b227d75e46f8017617
itizawa <itizawa.pen@gmail.com>
2024-09-15 22:30:56 +0900

    Change: 編集画面へ遷移できるようにした

--- apps/web/src/components/models/article/ArticlePaper/ArticlePaper.tsx
+++ apps/web/src/components/models/article/ArticlePaper/ArticlePaper.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import { Box, Link, Paper, Typography, useTheme } from '@mui/material';
+import { MoreVert } from '@mui/icons-material';
+import { Box, IconButton, Link, Paper, Typography, useTheme } from '@mui/material';
 import type { Article, Blog } from '@repo/types';
 import { format } from 'date-fns';
 import parse from 'html-react-parser';
+import { useRouter } from 'next/navigation';
 import type { FC } from 'react';
 import urlJoin from 'url-join';
+import { WrapperWithMenu } from '~/components/uiParts/WrapperWithMenu';
 import { generateSubDomainUrl } from '~/utils/generateSubDomainUrl';
 
 type Props = {
@@ -15,13 +18,33 @@ type Props = {
 
 export const ArticlePaper: FC<Props> = ({ blog, article }) => {
   const { palette } = useTheme();
+  const router = useRouter();
   return (
     <Paper key={article.id} variant='outlined' sx={{ p: 2, display: 'flex', flexDirection: 'column', rowGap: 0.5 }}>
-      <Link href={urlJoin(generateSubDomainUrl(blog.subDomain), article.id)} underline='hover' color='inherit'>
-        <Typography variant='h5' component='div'>
-          {article.title}
-        </Typography>
-      </Link>
+      <Box display='flex' alignItems='center' justifyContent='space-between'>
+        <Link href={urlJoin(generateSubDomainUrl(blog.subDomain), article.id)} underline='hover' color='inherit'>
+          <Typography variant='h5' component='div'>
+            {article.title}
+          </Typography>
+        </Link>
+        <WrapperWithMenu
+          menuItems={[
+            {
+              key: 'edit',
+              text: '編集',
+              onClick: () => router.push(urlJoin(generateSubDomainUrl(blog.subDomain), article.id, 'edit')),
+            },
+          ]}
+        >
+          {({ triggerMenu }) => {
+            return (
+              <IconButton onClick={triggerMenu}>
+                <MoreVert />
+              </IconButton>
+            );
+          }}
+        </WrapperWithMenu>
+      </Box>
       <Box display='flex' columnGap={2} sx={{ pb: 1, borderBottom: `1px solid ${palette.grey[500]}` }}>
         <Typography variant='caption' component='h6'>
           作成日：{format(article.createdAt, 'yyyy-MM-dd HH:mm')}
